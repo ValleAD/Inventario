03439e13c0deaf0faf9a3910397c36922d5562e8
Alvitronex <82683379+Alvitronex@users.noreply.github.com>
2021-11-25 17:31:37 -0600

    upload Database like reference

--- inventario/Bases de Datos/hospital.sql
+++ inventario/Bases de Datos/hospital.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-11-2021 a las 18:34:07
--- Versión del servidor: 10.4.17-MariaDB
--- Versión de PHP: 8.0.2
+-- Tiempo de generación: 26-11-2021 a las 00:28:09
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,70 +20,50 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `hospital`
 --
-CREATE DATABASE IF NOT EXISTS `hospital` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `hospital`;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `sol_bodega`
+-- Estructura de tabla para la tabla `producto`
 --
 
-CREATE TABLE `sol_bodega` (
-  `Codigo` int(11) NOT NULL,
-  `Descripcion` varchar(250) NOT NULL,
-  `U/M` text NOT NULL,
-  `Cantidad` int(11) NOT NULL,
-  `Cos_Unitario` double NOT NULL,
-  `Total` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `producto` (
+  `codProducto` int(11) NOT NULL,
+  `descripcion` varchar(200) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `u/m` varchar(2) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL,
+  `precioUnitario` decimal(6,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `sol_compra`
+-- Estructura de tabla para la tabla `solicitud_almacen`
 --
 
-CREATE TABLE `sol_compra` (
-  `Renglon` int(11) NOT NULL,
-  `Codigo_Producto` int(11) NOT NULL,
-  `Cod_Cat_Nac_Uni` int(11) NOT NULL,
-  `Des_Completa` int(11) NOT NULL,
-  `U/M` text NOT NULL,
-  `Cantidad` int(11) NOT NULL,
-  `Pre_Uni` int(11) NOT NULL,
-  `Monto_Total` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `solicitud_almacen` (
+  `idSolicitud_almacen` int(11) NOT NULL,
+  `Cantidad_Solicitada` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `Cantidad_Despachada` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `Producto_CodProducto` int(11) NOT NULL,
+  `Producto_Solicitud_Bodega_idBodega` int(11) NOT NULL,
+  `Producto_Fondo_Circulante_idSolicitud_fondo_Circulante` int(11) NOT NULL,
+  `Solicitud_Fondo_Circulante_Producto_Cod_Catalogo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `sol_fon_cir`
+-- Estructura de tabla para la tabla `solicitud_bodega`
 --
 
-CREATE TABLE `sol_fon_cir` (
-  `Numero` int(11) NOT NULL,
-  `Des_Mat_Ser_Sol` varchar(500) NOT NULL,
-  `U/M` text NOT NULL,
-  `Can_Sol` double NOT NULL,
-  `Costo-Estimado` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `sol_mat_alm`
---
-
-CREATE TABLE `sol_mat_alm` (
-  `Codigo` int(11) NOT NULL,
-  `U/M` text NOT NULL,
-  `Nombre_Articulo` varchar(250) NOT NULL,
-  `Can_Sol` int(11) NOT NULL,
-  `can_des` int(11) NOT NULL,
-  `Cos_Uni` double NOT NULL,
-  `Cos_Total` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `solicitud_bodega` (
+  `idBodega` int(11) NOT NULL,
+  `Cantidad` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `Total` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `Producto_CodProducto` int(11) NOT NULL,
+  `Producto_Solicitud_Bodega_idBodega` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
@@ -92,13 +72,68 @@ CREATE TABLE `sol_mat_alm` (
 --
 
 CREATE TABLE `vale` (
-  `Codigo` int(11) NOT NULL,
-  `Descripcion` varchar(250) NOT NULL,
-  `U/M` text NOT NULL,
-  `Cantidad` int(11) NOT NULL,
-  `Cos_Unitario` double NOT NULL,
-  `Total` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `idVale` int(11) NOT NULL,
+  `Cantidad` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `Total` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `Producto_CodProducto` int(11) NOT NULL,
+  `Producto_Solicitud_Bodega_idBodega` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`codProducto`);
+
+--
+-- Indices de la tabla `solicitud_almacen`
+--
+ALTER TABLE `solicitud_almacen`
+  ADD PRIMARY KEY (`idSolicitud_almacen`);
+
+--
+-- Indices de la tabla `solicitud_bodega`
+--
+ALTER TABLE `solicitud_bodega`
+  ADD PRIMARY KEY (`idBodega`);
+
+--
+-- Indices de la tabla `vale`
+--
+ALTER TABLE `vale`
+  ADD PRIMARY KEY (`idVale`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `codProducto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `solicitud_almacen`
+--
+ALTER TABLE `solicitud_almacen`
+  MODIFY `idSolicitud_almacen` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `solicitud_bodega`
+--
+ALTER TABLE `solicitud_bodega`
+  MODIFY `idBodega` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `vale`
+--
+ALTER TABLE `vale`
+  MODIFY `idVale` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
