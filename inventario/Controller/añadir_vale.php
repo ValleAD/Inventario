a438d9616d8d30712b1dfb8ce0bb23de96054829
Carlos Eduardo Del cid Gaitán <edudelcid2001@gmail.com>
2021-12-22 14:20:51 -0600

    update

--- inventario/Controller/añadir_vale.php
+++ inventario/Controller/añadir_vale.php
@@ -7,13 +7,14 @@ include '../Model/conexion.php';
 if (isset($_POST['submits'])) {
     $departamento = $_POST['departamento'];
     $odt = $_POST['odt'];
-    $cantidad = $_POST['cant'];
+    $codigo= $_POST['cod'];
+    $descripcion= $_POST['desc'];
+    $unidadmedida= $_POST['um'];
+    $stock = $_POST['cant'];
+    $precio= $_POST['cu'];
+    
 
-    $sql = "SELECT * FROM tb_productos WHERE coProductos AND descripcion AND unidad_medida AND stock AND precio";
-    $result = mysqli_query($conn, $sql);
-    if ($result) {
-        $sql = "INSERT INTO detalle_vale (codigo, descricion, unidad_medida, stock, precio)".
-        "SELECT coProductos AND descripcion AND unidad_medida AND stock AND precio FROM tb_productos WHERE codProductos";
+    $sql = "INSERT INTO tb_vale (departamento, odt) VALUES ('$departamento','$odt')";
         $result = mysqli_query($conn, $sql); 
         if ($result) {
             echo "<script> alert('Su solicitud fue guardada correctamente');
@@ -25,36 +26,13 @@ if (isset($_POST['submits'])) {
             location.href = '../form_vale.php';
             </script>
             ";
-          }
-        }
-}
-
-/* 
-
-if ($query1) {
-        $insertar1 = "INSERT INTO tb_vale (departamento, odt) VALUES ('$departamento', '$odt')";
-   $query1 = mysqli_query($conn, $insertar1); 
+         }
+         if($result){
+$sql  = "INSERT INTO detalle_vale (codigo,descripcion,unidadmedida,stock,precio) VALUES ('$codigo','$descripcion','$unidadmedida','$stock','$precio')";
+        $result = mysqli_query($conn, $sql);
+        
+         }
+    }          
 
 
-
-
-      }
-
-$insertar = "INSERT INTO detalle_vale(`codigo`, `descripcion`, `unidad_medida`, `stock`, `precio`)"
-        ."SELECT `codProductos`, `descripcion`, `unidad_medida`, `stock`, `precio` * FROM tb_productos WHERE codProductos" ;
-        $query = mysqli_query($conn, $insertar); 
-
-        if ($query) {
-            echo "<script> alert('Su solicitud fue guardada correctamente');
-            location.href = '../form_vale.php';
-            </script>
-            ";
-          }else {
-            echo "<script> alert('UUPS!! Algo no fue mal escrito');
-            location.href = '../form_vale.php';
-            </script>
-            ";
-          } 
-
-*/
 ?>
