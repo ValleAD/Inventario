5c996ad4692f2d5b6f8ced95b89717e4b775f4e7
Ángel Daniel Valle Pineda <angel.valle1208@gmail.com>
2022-01-02 18:21:48 -0600

    changes

--- inventario/menu.php
+++ inventario/menu.php
@@ -10,7 +10,7 @@ if(!isset($_SESSION['signin'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="styles/estilo_menu.css">
+    <link rel="stylesheet" type="text/css" href="styles/estilo_men.css">
     <link rel="stylesheet" type="text/css" href="Plugin/bootstrap/css/bootstrap.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
       <link rel="icon" type="image/png" sizes="32x32"  href="img/log.png">
