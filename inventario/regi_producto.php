45eefed22f074f8c714ae31a21abe3fc45737560
ByronPalacios21 <palaciosbyron582@gmail.com>
2022-01-19 13:26:12 -0600

    cambios

--- inventario/regi_producto.php
+++ inventario/regi_producto.php
@@ -130,7 +130,7 @@ form{
     <hr />
     
     <div class="text-center">
-        <button class="btn btn-success btn-lg" style="margin-bottom: 2%;">Guardar</button>  
+        <button class="btn btn-success btn-lg" name="submit" style="margin-bottom: 2%;">Guardar</button>  
         <a id="ver" class="btn btn-lg" href="vistaProductos.php">Ver Productos</a>
          <style>
                #ver{
