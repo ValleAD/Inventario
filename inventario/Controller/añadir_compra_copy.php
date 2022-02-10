c7a0dedf0b0fb1150cbb26bebe2e251c94eac6a7
Carlos Eduardo Del cid Gaitán <82596224+CarlosUD@users.noreply.github.com>
2022-02-10 14:16:26 -0600

    Update añadir_compra_copy.php

--- inventario/Controller/añadir_compra_copy.php
+++ inventario/Controller/añadir_compra_copy.php
@@ -19,12 +19,12 @@ include '../Model/conexion.php';
       $query = mysqli_query($conn, $insert);
       if ($query)  {
         echo "<script> alert('Su producto fue registrado correctamente')
-        location.href = '../Detalle_Compra.php';
+        location.href = '../solicitudes_compra.php';
         </script>
         ";
         }else {
         echo "<script> alert('UUPS!! Algo no fue mal escrito')
-        location.href = '../form_compra.php';
+        location.href = '../dt_compra_copy.php';
         </script>
         ";
         }
