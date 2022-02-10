caa9317099499796afbf904fe8549df6101937b9
Alvitronex <82683379+Alvitronex@users.noreply.github.com>
2022-02-10 08:48:25 -0600

    menu

--- inventario/reporte_producto.php
+++ inventario/reporte_producto.php
@@ -20,22 +20,29 @@ die();
 <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="styles/estilos_tablas.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="icon" type="image/png" sizes="32x32"  href="img/log.png">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     <link rel="stylesheet" type="text/css" href="Plugin/bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" href="Plugin/bootstap-icon/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="Plugin/bootstap-icon/fontawesome.all.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
-<title>Reportes de Productos</title>
+         <link rel="stylesheet" href="Plugin/bootstap-icon/bootstrap-icons.min.css">
+      <link rel="stylesheet" href="Plugin/bootstap-icon/fontawesome.all.min.css">
+      <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
+  
+
+    <title>Productos</title>
 </head>
 <body>
-    <section id="act">
+    <div class="container">
+      <table class="table">
         
-           <h1 style="margin-top:5px; text-align: center;">Reportes de Productos</h1>
+           <h1 style="margin-top:5px; text-align: center;">solicitud de Vale</h1>
+
+
+
+
 <style>
-  #act {
+ #act {
     margin-top: 0.5%;
     margin-right: 2%;
     margin-left: 2%;
@@ -58,50 +65,53 @@ die();
         #ver:active{
         transform: translateY(5px);
         } 
+         #act {
+    margin-top: 0.5%;
+    margin-right: 2%;
+    margin-left: 2%;
+  }
     </style>
-
-
-
+</table>
+</div>
+<section id="act">
+   
+<br><br>
+     <form style="margin: 0%;position: 0; background: transparent;" method='POST' action="form_compra.php">
        <table class="table">
             <thead>
               <tr id="tr">
-                <th style="text-align:center">Categoría</th>
-                <th style="text-align:center">Código</th>
-                <th style="text-align:center">Cod. de Catálogo</th>
-                <th style="text-align:center">Nombre</th>
-                <th style="text-align:center">Descripción Completa</th>
-                <th style="text-align:center">U/M</th>
-                <th style="text-align:center">Cantidad</th>
-                <th style="text-align:center">Costo Unitario</th>
-                <th style="text-align:center">Formularios</th>
-                <th style="text-align:center">Fecha Registro</th>
+                <th style="width: 175%;">Categoría</th>
+                <th style="width: 95%;">Código</th>
+                <th style="width: 135%;">Catálogo</th>
+                <th style="width: 200%;">Nombre</th>
+                <th style="width: 225%;">Descripción Completa</th>
+                <th style="width: 175%; text-align: center;">U/M</th>
+                <th style="width: 115%;">Cantidad</th>
+                <th style="width: 175%;">Costo Unitario</th>
+                <th style="width: 145%;">Fecha Registro</th>
+                <th style="width: 145%;" align="center"><button  name='solicitar' class='btn btn-success btn-sm text-center'  data-bs-toggle="tooltip" data-bs-placement="top" title="Solicitar">Solicitar</button> </th>
+               
               </tr>
 
-              <tr>
-   <td id="td" colspan="10">
+              <tr> <td id="td" colspan="11">
                 <h4 align="center">No se encontraron resultados 😥</h4></td>
               </tr>
             </thead>
 
             <tbody>
- <?php
- include 'Model/conexion.php';
- $por_pagina = 6;
- if (isset($_GET['pagina'])) {
-    $pagina = $_GET['pagina'];
- }else{
-    $pagina =1;
- }
- $empieza = ($pagina-1) * $por_pagina;
 
-    
-   $sql = "SELECT * FROM reporte_articulos LIMIT $empieza,$por_pagina";
+ <?php
+    include 'Model/conexion.php';
+
+
+    //    $sql = "SELECT * FROM tb_productos";
+    $sql = "SELECT codProductos, categoria, catalogo, nombre, descripcion, unidad_medida, SUM(stock), precio, fecha_registro FROM tb_productos GROUP BY precio, codProductos";
     $result = mysqli_query($conn, $sql);
 
-    while ($productos = mysqli_fetch_assoc($result)){?>
+    while ($productos = mysqli_fetch_array($result)){?>
 
        
-             
+               
 
 
 <style type="text/css">
@@ -117,41 +127,45 @@ die();
     <td data-label="Categoría"><?php  echo $productos['categoria']; ?></td>
       <td data-label="Codigo"><?php  echo $productos['codProductos']; ?></td>
       <td data-label="Codificación de catálogo"><?php  echo $productos['catalogo']; ?></td>
-      <td data-label="Codigo"><?php  echo $productos['nombre']; ?></td>
-      <td data-label="Descripción Completa"><textarea  style="background:transparent; border: none; color: black;"  readonly name="" id="" cols="15" rows="2" class="form-control"><?php  echo $productos['descripcion']; ?></textarea></td>
+      <td data-label="Nombre"><?php  echo $productos['nombre']; ?></td>
+      <td data-label="Descripción Completa"><textarea style="background:transparent; border: none; color: black;" cols="10" rows="1" readonly name="" id="" cols="10" rows="3" class="form-control"><?php  echo $productos['descripcion']; ?></textarea></td>
       <td data-label="Unidad De Medida" style="text-align: center;"><?php  echo $productos['unidad_medida']; ?></td>
-      <td data-label="Cantidad" style="text-align: center;"><?php  echo $productos['stock']; ?></td>
+      <td data-label="Cantidad" style="text-align: center;"><?php  echo $productos['SUM(stock)']; ?></td>
       <td data-label="Costo Unitario">$<?php  echo $productos['precio']; ?></td>
-      <td data-label="Costo Unitario"><?php  echo $productos['campo']; ?></td>
       <td data-label="Fecha Registro"><?php  echo $productos['fecha_registro']; ?></td>
-      
-
-    
-    </tr>
-
+      <td data-label="solicitar" align="center">
+                    
+          <input type="checkbox" name="id[]"  value="<?php  echo $productos['codProductos']; ?>">             
+         
+      </td>
+     
 <?php } ?> 
 
             </tbody>
         </table>
- <p style="margin-top: 2%;"></p>
-<?php 
- $sql = "SELECT * FROM reporte_articulos";
-    $result = mysqli_query($conn, $sql);
-$total_registro = mysqli_num_rows($result);
-$total_pagina = ceil($total_registro / $por_pagina);
-
-echo "<nav aria-label='Page navigation example'>
-  <ul class='pagination justify-content-end'><li class='page-item '><a class='page-link' href='reporte_producto.php?pagina= 1'>".'Primera'."</a><li>";
-for ($i=1; $i <=$total_pagina; $i++) { 
-    echo "<li class='page-item '><a class='page-link ' href='reporte_producto.php?pagina=".$i."'>".$i."</a></li>";
-}
-echo "<li class='page-item'><a class='page-link' href='reporte_producto.php?pagina=$total_pagina'>".'Ultima'."</a><li></ul></nav>";
-?>
-
+</form>
 </section>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-
+    <script>
+$(function(){
+    var textArea = $('#content'),
+    hiddenDiv = $(document.createElement('div')),
+    content = null;
+    
+    textArea.addClass('noscroll');
+    hiddenDiv.addClass('hiddendiv');
+    
+    $(textArea).after(hiddenDiv);
+    
+    textArea.on('keyup', function(){
+        content = $(this).val();
+        content = content.replace(/\n/g, '<br>');
+        hiddenDiv.html(content + '<br class="lbr">');
+        $(this).css('height', hiddenDiv.height());
+    });
+});
+</script>
 <script type="text/javascript">
 function confirmaion(e) {
     if (confirm("¿Estas seguro que deseas Eliminar este registro?")) {
@@ -161,7 +175,6 @@ function confirmaion(e) {
         e.preventDefault();
     }
 }
-let linkDelete =document.querySelectorAll("delete");
 </script>
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
 </body>
