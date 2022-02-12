0fbd6219c7596586dbc433bbdc0660c4ca5f2274
CarlosUD <edudelcid2001@gmail.com>
2022-02-12 00:43:29 -0600

    Update vistaProductos.php

--- inventario/vistaProductos.php
+++ inventario/vistaProductos.php
@@ -19,7 +19,7 @@ die();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Buscar Producto</title>
+    <title>VistaProductos</title>
    
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
