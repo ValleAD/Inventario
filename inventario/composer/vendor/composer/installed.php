2df61fc432159c1c0e45482c283ec38f89b34f8b
Alvitronex <82683379+Alvitronex@users.noreply.github.com>
2022-02-14 13:53:14 -0600

    dompdf and mpdf

--- inventario/composer/vendor/composer/installed.php
+++ inventario/composer/vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '4b47e3dbe7c7ae9b01b13372c13e75e145eab1de',
+        'reference' => '21aa1718438060047826f404581ba0f852710fa8',
         'name' => '__root__',
         'dev' => true,
     ),
@@ -16,7 +16,7 @@
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '4b47e3dbe7c7ae9b01b13372c13e75e145eab1de',
+            'reference' => '21aa1718438060047826f404581ba0f852710fa8',
             'dev_requirement' => false,
         ),
         'aws/aws-sdk-php' => array(
@@ -168,6 +168,24 @@
             'replaced' => array(
                 0 => 'v3.9.3',
             ),
+        ),
+        'mpdf/mpdf' => array(
+            'pretty_version' => 'v6.1.3',
+            'version' => '6.1.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mpdf/mpdf',
+            'aliases' => array(),
+            'reference' => '7f138bf7508eac895ac2c13d2509b056ac7e7e97',
+            'dev_requirement' => false,
+        ),
+        'setasign/fpdi' => array(
+            'pretty_version' => '1.6.2',
+            'version' => '1.6.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../setasign/fpdi',
+            'aliases' => array(),
+            'reference' => 'a6ad58897a6d97cc2d2cd2adaeda343b25a368ea',
+            'dev_requirement' => false,
         ),
         'symfony/event-dispatcher' => array(
             'pretty_version' => 'v2.8.52',
