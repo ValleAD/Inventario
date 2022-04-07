de7e5ed77c7a764b45799b951f8d6849e31ff9e4
CarlosUD <edudelcid2001@gmail.com>
2022-04-07 17:34:09 -0600

    Update productos.php

--- inventario/log/Invitado/vale/productos.php
+++ inventario/log/Invitado/vale/productos.php
@@ -8,7 +8,7 @@
      <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
          <link rel="stylesheet" type="text/css" href="../../../styles/estilo_men.css">
       <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../../../Plugin/bootstrap/css/bootstrap.css">
 
     <!--  Datatables  -->
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.20/datatables.min.css"/>  
